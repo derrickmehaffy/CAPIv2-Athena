ba82d510def6de473c54cade0aede1824420a4b0
Derrick Mehaffy <derrickmehaffy@gmail.com>
2019-04-17 06:39:23 -0700

    Some basic structure and all types added, using CAPIv2 Dev

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import { Admin, Resource } from 'react-admin';
 import simpleRestProvider from './utils/ra-strapi-rest';
 
 import {
-    typelist
+    typeList,
+    typeGuardianList,
+    typeThargoidList
 } from './models/types';
 
-const dataProvider = simpleRestProvider('http://localhost:1337');
+import Dashboard from './pages/dashboard';
+import NotFound from './pages/notfound';
+
+const dataProvider = simpleRestProvider('https://api.canonn.tech:2083');
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="aptypes" list={typelist} />
-        <Resource name="bmtypes" list={typelist} />
-        <Resource name="bttypes" list={typelist} />
+    <Admin dashboard={Dashboard} catchAll={NotFound} title="Canonn APIv2" dataProvider={dataProvider}>
+        <Resource name="aptypes" list={typeList} />
+        <Resource name="bmtypes" list={typeList} />
+        <Resource name="bttypes" list={typeList} />
+        <Resource name="cstypes" list={typeList} />
+        <Resource name="fgtypes" list={typeList} />
+        <Resource name="fmtypes" list={typeList} />
+        <Resource name="grtypes" list={typeGuardianList} />
+        <Resource name="gstypes" list={typeGuardianList} />
+        <Resource name="gvtypes" list={typeList} />
+        <Resource name="gytypes" list={typeList} />
+        <Resource name="lstypes" list={typeList} />
+        <Resource name="tbtypes" list={typeThargoidList} />
+        <Resource name="twtypes" list={typeList} />
     </Admin>
 );
 
--- src/models/types.js
+++ src/models/types.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { List, Datagrid, TextField } from 'react-admin';
+import { List, Datagrid, TextField, NumberField } from 'react-admin';
 
-export const typelist = (props) => (
-    <List {...props}>
+// Default Types
+export const typeList = (props) => (
+    <List {...props} sort={{ field: 'id', order: 'ASC' }}>
         <Datagrid>
-            <TextField source="id" />
-            <TextField source="type" />
-            <TextField source="journalName" />
-            <TextField source="journalID" />
+            <NumberField source="id" label="ID" textAlign="Left"/>
+            <TextField source="type" label="Type"/>
+            <TextField source="journalName" label="Journal Name"/>
+            <NumberField source="journalID" label="Journal ID" textAlign="Left"/>
+        </Datagrid>
+    </List>
+);
+
+// Guardian Types
+export const typeGuardianList = (props) => (
+    <List {...props} sort={{ field: 'id', order: 'ASC' }}>
+        <Datagrid>
+            <NumberField source="id" label="ID" textAlign="Left"/>
+            <TextField source="type" label="Type"/>
+        </Datagrid>
+    </List>
+);
+
+// Thargoid Barnacle Types
+export const typeThargoidList = (props) => (
+    <List {...props} sort={{ field: 'id', order: 'ASC' }}>
+        <Datagrid>
+            <NumberField source="id" label="ID" textAlign="Left"/>
+            <TextField source="type" label="Type"/>
+            <NumberField source="metaAlloyCount" label="Meta Alloy" textAlign="Left"/>
         </Datagrid>
     </List>
 );
